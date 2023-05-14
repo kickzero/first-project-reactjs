d8868b8e4432ec7f68c0b4afb923e5cfe7cb194b
ltluan <66855902+kickzero@users.noreply.github.com>
2023-05-14 20:46:53 +0700

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -4,14 +4,14 @@ import { ACCESS_TOKEN } from "../constants";
 const API = {
   call() {
     return axios.create({
-      baseURL: 'http://wp-api.test/wp-json'
+      baseURL: 'http://blogpony.azdigi.blog/'
     });
   },
   callWithToken(token) {
     if(!token) token = localStorage.getItem(ACCESS_TOKEN);
     
     return axios.create({
-      baseURL: 'http://wp-api.test/wp-json',
+      baseURL: 'http://blogpony.azdigi.blog/',
       headers: { Authorization: 'Bearer ' + token},
     });
   }
